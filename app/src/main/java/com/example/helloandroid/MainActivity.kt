6c642af13295bb57f9446131cff5241379cdabcf
Dayana Alves de Souza <108834440+dayasouza@users.noreply.github.com>
2024-07-30 08:12:27 -0300

    Update MainActivity.kt
    teste 1.1

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     // Teste 1
+    //teste 1.1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
